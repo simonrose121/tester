61da07a92a8c33515f2aeed0e87a1bb0ad624088
Simon Rose <simonrose121@gmail.com>
2015-11-23 19:06:14 +0000

    Update questions

--- client/js/questions.constants.js
+++ client/js/questions.constants.js
@@ -49,6 +49,102 @@
 					},
 				],
 				"CorrectAnswer": 3
+			},
+			{
+				"Id": 3,
+				"Question" : "Match one of the shapes below with this shape",
+				"Image" : "q3.png",
+				"Answers": [
+					{
+						"id": 1,
+						"src": "q3a1.png"
+					},
+					{
+						"id": 2,
+						"src": "q3a2.png"
+					},
+					{
+						"id": 3,
+						"src": "q3a3.png"
+					},
+					{
+						"id": 4,
+						"src": "q3a4.png"
+					},
+				],
+				"CorrectAnswer": 4
+			},
+			{
+				"Id": 4,
+				"Question" : "Match one of the shapes below with this shape",
+				"Image" : "q4.png",
+				"Answers": [
+					{
+						"id": 1,
+						"src": "q4a1.png"
+					},
+					{
+						"id": 2,
+						"src": "q4a2.png"
+					},
+					{
+						"id": 3,
+						"src": "q4a3.png"
+					},
+					{
+						"id": 4,
+						"src": "q4a4.png"
+					},
+				],
+				"CorrectAnswer": 1
+			},
+			{
+				"Id": 5,
+				"Question" : "Match one of the shapes below with this shape",
+				"Image" : "q5.png",
+				"Answers": [
+					{
+						"id": 1,
+						"src": "q5a1.png"
+					},
+					{
+						"id": 2,
+						"src": "q5a2.png"
+					},
+					{
+						"id": 3,
+						"src": "q5a3.png"
+					},
+					{
+						"id": 4,
+						"src": "q5a4.png"
+					},
+				],
+				"CorrectAnswer": 1
+			},
+			{
+				"Id": 6,
+				"Question" : "Match one of the shapes below with this shape",
+				"Image" : "q6.png",
+				"Answers": [
+					{
+						"id": 1,
+						"src": "q6a1.png"
+					},
+					{
+						"id": 2,
+						"src": "q6a2.png"
+					},
+					{
+						"id": 3,
+						"src": "q6a3.png"
+					},
+					{
+						"id": 4,
+						"src": "q6a4.png"
+					},
+				],
+				"CorrectAnswer": 1
 			}
 		]);
 })();
