ab64902221c6cd0fef8344438d61d11587d5a4f1
Simon Rose <simonrose121@gmail.com>
2015-11-24 16:34:58 +0000

    Adjust question javascript object format

--- client/js/questions.constants.js
+++ client/js/questions.constants.js
@@ -1,48 +1,53 @@
 (function() {
 	angular
 		.module('tester')
-		.constant('questions', [
-			{
-				"Id": 1,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q1.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
+		.constant('questions', {
+			"types": {
+				"match": "Match one of the shapes below with this shape",
 			},
-			{
-				"Id": 2,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q2.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
-			},
-			{
-				"Id": 3,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q3.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
-			},
-			{
-				"Id": 4,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q4.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
-			},
-			{
-				"Id": 5,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q5.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
-			},
-			{
-				"Id": 6,
-				"Question" : "Match one of the shapes below with this shape",
-				"Image" : "q6.png",
-				"Answers": 4,
-				"CorrectAnswer": 1
-			}
-		]);
+			"questions": [
+				{
+					"Id": 1,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 2,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 3,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 4,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 5,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 6,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+				{
+					"Id": 7,
+					"Type": "match",
+					"Answers": 4,
+					"CorrectAnswer": 1
+				},
+			]
+		});
 })();
--- client/js/tester.controller.js
+++ client/js/tester.controller.js
@@ -13,6 +13,7 @@
 
 		vm.currentQuestion = null;
 		vm.answers = [];
+		vm.question = "";
 		vm.index = 0;
 		vm.userId = 1;
 		vm.userIdField = null;
@@ -28,15 +29,19 @@
 		vm.timerInterval = timerInterval;
 
 		function displayNextQuestion() {
+			vm.answers.length = 0;
+			vm.question = "";
+
 			// get next question in array
-			vm.currentQuestion = questions[vm.index];
+			vm.currentQuestion = questions.questions[vm.index];
 
 			for (var i = 1; i <= vm.currentQuestion.Answers; i++) {
 				vm.answers.push(i);
 			}
 
 			shuffle(vm.answers);
-			console.log(vm.answers);
+
+			vm.question = questions.types[vm.currentQuestion.Type];
 		}
 
 		function progressBar() {
@@ -65,8 +70,8 @@
 			logService.postAnswer(vm.userId, vm.currentQuestion.Id, answer, correct);
 
 			vm.index++;
-			//TODO: REMOVE
-			if (vm.index == 6) {
+
+			if (vm.index == 7) {
 				vm.index = 0;
 			}
 
