060461812f8dbde26c835498add35b46c1330be4
Simon Rose <simonrose121@gmail.com>
2015-12-02 11:54:21 +0000

    Comments on questions file to indicate question count

--- client/js/questions.constants.js
+++ client/js/questions.constants.js
@@ -10,60 +10,70 @@
 			},
 			"questions": [
 				// Odd
+				// 1
 				{
 					"pictureId": 8,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 2
 				{
 					"pictureId": 9,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 3
 				{
 					"pictureId": 10,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 4
 				{
 					"pictureId": 11,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 5
 				{
 					"pictureId": 12,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 6
 				{
 					"pictureId": 13,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 7
 				{
 					"pictureId": 14,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 8
 				{
 					"pictureId": 15,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 9
 				{
 					"pictureId": 35,
 					"type": "odd",
 					"answers": 2,
 					"correct": 1
 				},
+				// 10
 				{
 					"pictureId": 36,
 					"type": "odd",
@@ -71,156 +81,184 @@
 					"correct": 1
 				},
 				// Match
+				// 1
 				{
 					"pictureId": 1,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 2
 				{
 					"pictureId": 2,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 3
 				{
 					"pictureId": 3,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 4
 				{
 					"pictureId": 4,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 5
 				{
 					"pictureId": 5,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 6
 				{
 					"pictureId": 6,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 7
 				{
 					"pictureId": 7,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 8
 				{
 					"pictureId": 32,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 9
 				{
 					"pictureId": 33,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// 10
 				{
 					"pictureId": 34,
 					"type": "match",
 					"answers": 4,
 					"correct": 1
 				},
+				// Missing
+				// 1
 				{
 					"pictureId": 16,
 					"type": "missing",
 					"answers": 2,
 					"correct": 2
 				},
+				// 2
 				{
 					"pictureId": 17,
 					"type": "missing",
 					"answers": 2,
 					"correct": 1
 				},
+				// 3
 				{
 					"pictureId": 18,
 					"type": "missing",
 					"answers": 3,
 					"correct": 2
 				},
+				// 4
 				{
 					"pictureId": 19,
 					"type": "missing",
 					"answers": 3,
 					"correct": 3
 				},
+				// 5
 				{
 					"pictureId": 20,
 					"type": "missing",
 					"answers": 3,
 					"correct": 1
 				},
+				// 6
 				{
 					"pictureId": 21,
 					"type": "missing",
 					"answers": 3,
 					"correct": 1
 				},
+				// 7
 				{
 					"pictureId": 22,
 					"type": "missing",
 					"answers": 3,
 					"correct": 3
 				},
+				// 8
 				{
 					"pictureId": 23,
 					"type": "missing",
 					"answers": 2,
 					"correct": 2
 				},
+				// Sequence
+				// 1
 				{
 					"pictureId": 24,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 1
 				},
+				// 2
 				{
 					"pictureId": 25,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 3
 				},
+				// 3
 				{
 					"pictureId": 26,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 1
 				},
+				// 4
 				{
 					"pictureId": 27,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 3
 				},
+				// 5
 				{
 					"pictureId": 28,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 1
 				},
+				// 6
 				{
 					"pictureId": 29,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 2
 				},
+				// 7
 				{
 					"pictureId": 30,
 					"type": "sequence",
 					"answers": 3,
 					"correct": 1
 				},
+				// 8
 				{
 					"pictureId": 31,
 					"type": "sequence",
